2d57e00219cadc15245e33b158b4fd6d1ca3ca52
Maxime OUAIRY <maxime.ouairy-ext@consensys.net>
2026-04-13 07:48:15 +0200

    fix: tempo disable confirm button until gas autoselect

--- ui/pages/confirmations/hooks/gas/useIsGaslessLoading.ts
+++ ui/pages/confirmations/hooks/gas/useIsGaslessLoading.ts
@@ -1,28 +1,73 @@
 import { useSelector } from 'react-redux';
 import { TransactionMeta } from '@metamask/transaction-controller';
+import { useMemo } from 'react';
 import { useConfirmContext } from '../../context/confirm';
 import { getUseTransactionSimulations } from '../../../../selectors';
 import { useHasInsufficientBalance } from '../useHasInsufficientBalance';
+import { NATIVE_TOKEN_ADDRESS } from '../../../../../shared/constants/transaction';
 import { useIsGaslessSupported } from './useIsGaslessSupported';
 
 export function useIsGaslessLoading() {
   const { currentConfirmation: transactionMeta } =
     useConfirmContext<TransactionMeta>();
 
-  const { gasFeeTokens } = transactionMeta ?? {};
+  const { gasFeeTokens, excludeNativeTokenForFee, selectedGasFeeToken } =
+    transactionMeta ?? {};
 
-  const { isSupported: isGaslessSupported, pending } = useIsGaslessSupported();
+  const {
+    isSupported: isGaslessSupported,
+    pending: isGaslessSupportedPending,
+  } = useIsGaslessSupported();
   const isSimulationEnabled = useSelector(getUseTransactionSimulations);
 
   const { hasInsufficientBalance } = useHasInsufficientBalance();
 
-  const isGaslessSupportedFinished = !pending && isGaslessSupported;
-
-  const isGaslessLoading =
-    isSimulationEnabled &&
-    isGaslessSupportedFinished &&
-    hasInsufficientBalance &&
-    !gasFeeTokens;
-
+  const isGaslessLoading = useMemo(() => {
+    if (isGaslessSupportedPending) {
+      return true;
+    }
+    if (!isGaslessSupported) {
+      return false;
+    }
+    if (!isSimulationEnabled || !hasInsufficientBalance) {
+      return false;
+    }
+    /*
+     * Sometimes useAutomaticGasFeeTokenSelect needs to have time to run
+     * for the correct available fee token to be assigned.
+     * This is the case on Tempo when we 'suggest' a default fee token
+     * but this is not one of the available token.
+     * Not doing this would fallback on using the native token, which makes it
+     * less painful on other chains, but means tx failure on Tempo.
+     * By using `excludeNativeTokenForFee` as guard, we limit regression risks on
+     * other networks/flows while solving this issue for Tempo.
+     * Without this, clicking too fast on "confirm" while the tx is building would
+     * be possible and would cause a tx failure.
+     */
+    if (excludeNativeTokenForFee) {
+      const hasSelectedGasFeeTokenInconsistentWithAvailableGasFeeTokens =
+        selectedGasFeeToken &&
+        selectedGasFeeToken !== NATIVE_TOKEN_ADDRESS &&
+        gasFeeTokens &&
+        !gasFeeTokens.find(
+          ({ tokenAddress }) =>
+            tokenAddress.toLocaleLowerCase() ===
+            selectedGasFeeToken?.toLocaleLowerCase(),
+        );
+      return (
+        !gasFeeTokens ||
+        Boolean(hasSelectedGasFeeTokenInconsistentWithAvailableGasFeeTokens)
+      );
+    }
+    return !gasFeeTokens;
+  }, [
+    isGaslessSupportedPending,
+    isGaslessSupported,
+    isSimulationEnabled,
+    hasInsufficientBalance,
+    gasFeeTokens,
+    selectedGasFeeToken,
+    excludeNativeTokenForFee,
+  ]);
   return { isGaslessLoading };
 }
